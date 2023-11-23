151d96e98dfebdeb6896a23f98ab89d2edcbddae
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2023-11-23 16:23:22 +0000

    now can search and scrape user inputted values

--- scraper.py
+++ scraper.py
@@ -1,34 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=cars&_sacat=0&rt=nc&LH_Auction=1"
+class Scraper:
+    def __init__(self, url):
+        self.url = url
+        print("starting Scraper")
 
-def get_data(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
 
-    return(soup)
+    def get_data(url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-def parse(soup):
-    products_list = []
-    results = soup.find_all('div', {'class': 's-item__info clearfix'})
-    for item in results:
-        if (item.find('a', {'class': 's-item__link'})['href'] != "https://ebay.com/itm/123456?hash=item28caef0a3a:g:E3kAAOSwlGJiMikD&amdata=enc%3AAQAHAAAAsJoWXGf0hxNZspTmhb8%2FTJCCurAWCHuXJ2Xi3S9cwXL6BX04zSEiVaDMCvsUbApftgXEAHGJU1ZGugZO%2FnW1U7Gb6vgoL%2BmXlqCbLkwoZfF3AUAK8YvJ5B4%2BnhFA7ID4dxpYs4jjExEnN5SR2g1mQe7QtLkmGt%2FZ%2FbH2W62cXPuKbf550ExbnBPO2QJyZTXYCuw5KVkMdFMDuoB4p3FwJKcSPzez5kyQyVjyiIq6PB2q%7Ctkp%3ABlBMULq7kqyXYA"):
-            product = {
-                'title': item.find('div', {'class': 's-item__title'}).text,
-                'soldprice': float(item.find('span', {'class': 's-item__price'}).text.replace('£', '').replace(',', '').strip()),
-                'timeleft': item.find('span', {'class': 's-item__time-left'}).text,
-                'bids': item.find('span', {'class': 's-item__bids'}).text,
-                'link': item.find('a', {'class': 's-item__link'})['href']
-            }
-            products_list.append(product)
-    return products_list
+        return(soup)
 
-'''def output(products_list):
-    productsdf = pd.DataFrame(products_list)
-    productsdf.to_csv('output.csv', index=False)
-    print("saved to csv")
-    return'''
+    def parse(soup):
+        products_list = []
+        results = soup.find_all('div', {'class': 's-item__info clearfix'})
+        for item in results:
+            if (item.find('a', {'class': 's-item__link'})['href'] != "https://ebay.com/itm/123456?hash=item28caef0a3a:g:E3kAAOSwlGJiMikD&amdata=enc%3AAQAHAAAAsJoWXGf0hxNZspTmhb8%2FTJCCurAWCHuXJ2Xi3S9cwXL6BX04zSEiVaDMCvsUbApftgXEAHGJU1ZGugZO%2FnW1U7Gb6vgoL%2BmXlqCbLkwoZfF3AUAK8YvJ5B4%2BnhFA7ID4dxpYs4jjExEnN5SR2g1mQe7QtLkmGt%2FZ%2FbH2W62cXPuKbf550ExbnBPO2QJyZTXYCuw5KVkMdFMDuoB4p3FwJKcSPzez5kyQyVjyiIq6PB2q%7Ctkp%3ABlBMULq7kqyXYA"):
+                product = {
+                    'title': item.find('div', {'class': 's-item__title'}).text,
+                    'soldprice': float(item.find('span', {'class': 's-item__price'}).text.replace('£', '').replace(',', '').strip()),
+                    'timeleft': item.find('span', {'class': 's-item__time-left'}).text,
+                    'bids': item.find('span', {'class': 's-item__bids'}).text,
+                    'link': item.find('a', {'class': 's-item__link'})['href']
+                }
+                products_list.append(product)
+        return products_list
 
-soup = get_data(url)
-print(parse(soup)[0].get('title'))
+    def run(self):
+        soup = Scraper.get_data(self.url)
+        print(Scraper.parse(soup)[0].get('title'))
+        print(Scraper.parse(soup)[0].get('soldprice'))
+        print(Scraper.parse(soup)[0].get('timeleft'))
+        print(Scraper.parse(soup)[0].get('bids'))
+        print(Scraper.parse(soup)[0].get('link'))
+
+''' test_scraper = Scraper("https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=perfume&_sacat=0&rt=nc&LH_Auction=1")
+test_scraper.run() '''
--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from scraper import Scraper
 
 # constants
 BG_GRAY = "lightgray"
@@ -6,9 +7,25 @@ BG_COLOR = "white"
 TEXT_COLOR = "black"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
+url1 = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw="
+url2 = "&_sacat=0&rt=nc&LH_Auction=1"
+
+def close_app():
+    window.destroy()
+
+def search():
+    # get inputs to scrape
+    # check what value has been inputted into the entry box
+    # use that input to create the url to be passed into the scraper
+    # start scraper function
+    search_product1 = str(search_product_entry.get()) # perfume
+    print("search: " + search_product1)
+    scraper1 = Scraper(url1 + search_product1 + url2) # concatenate strings to include search value
+    scraper1.run()
+
 
 window = Tk()
-# window.state("zoomed") # makes full screen with title bar and taskbar
+window.state("zoomed") # makes full screen with title bar and taskbar
 window.title("Ecommerce-Web-Scraper")
 
 # creating grid for gui
@@ -22,6 +39,26 @@ frame_bottom.grid(row=2, column=0)
 
 header = Label(frame_header, text="Ecommerce Web Scraper", bg='grey', fg='black', height='3', width='50', font=("Helvetica 16 bold"))
 header.grid(row=0, column=0)
+
+frame_main_1 = Frame(frame_centre, borderwidth=2, relief='sunken')
+frame_main_2 = Frame(frame_centre, borderwidth=2, relief='raised')
+
+search_product = Label(frame_main_1, text="Search product: ")
+# NEED TO ADD SEND BUTTON
+search_product1 = StringVar() # ??
+search_product_entry = Entry(frame_main_1, textvariable = search_product1, width=40) # width=40 changes the width of the entry box
+search_button = Button(frame_main_1, text="Search", command=search, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
+
+
+#adding send button
+close_button = Button(frame_bottom, text="Close", command=close_app, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
+close_button.grid(column=0, row=0, sticky='w', padx=100, pady=2)
+# adding widgets to the grid
+frame_main_1.pack(fill='x', pady=2)
+frame_main_2.pack(fill='x', pady=2)
+search_product.pack(side='left')
+search_product_entry.pack(side='left', padx=1)
+search_button.pack(side='left', padx=1)
 
 
 window.configure(width=500, height=750, bg=BG_COLOR) # window.geometry("500x750")
