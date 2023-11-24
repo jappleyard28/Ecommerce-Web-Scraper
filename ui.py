23a346b9902c64566e5d3a66f3d132ab0bdf8ae5
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2023-11-24 06:32:25 +0000

    cleaned up code

--- ui.py
+++ ui.py
@@ -56,8 +56,6 @@ def create_table(data_list):
     # ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91') = TUPLE
     # ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91'), ('title 92', 'price 92', 'time_left 92', 'bids 92', 'link 92') = LIST
     products = []
-    # for i in data_list:
-    #     products.append(('title ', 'price ', 'time_left ', 'bids ', 'link '))
     
     for i in range(len(data_list)):
         products.append((data_list[i].get('title'), data_list[i].get('price'), data_list[i].get('time_left'), data_list[i].get('bids'), data_list[i].get('link')))
@@ -97,13 +95,14 @@ if __name__ == "__main__":
     frame_main_2 = Frame(frame_centre, borderwidth=2, relief='raised')
 
     search_product = Label(frame_main_1, text="Search product: ")
-    # NEED TO ADD SEND BUTTON
-    search_product1 = StringVar() # ??
+    
+    # search button
+    search_product1 = StringVar()
     search_product_entry = Entry(frame_main_1, textvariable = search_product1, width=40) # width=40 changes the width of the entry box
     search_button = Button(frame_main_1, text="Search", command=search, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
 
 
-    #adding send button
+    # adding send button
     close_button = Button(frame_main_1, text="Close", command=close_app, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
 
     data = [{'title': 'car 1', 'price': '1', 'time_left': 'time 1', 'bids': "1", 'link': 'link test 1'}, {'title': 'car 2', 'price': '2', 'time_left': 'time 2', 'bids': "2", 'link': 'link test 2'}] 
@@ -121,7 +120,4 @@ if __name__ == "__main__":
 
     window.configure(width=500, height=750, bg=BG_COLOR) # window.geometry("500x750")
 
-
-    # all widgets will be here
-    # Execute Tkinter
     window.mainloop()
