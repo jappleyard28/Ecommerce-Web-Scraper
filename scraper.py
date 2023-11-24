76647a6fbf8001b8732e7d4db917f0a73b8ed1b8
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2023-11-24 06:24:59 +0000

    table can now be updated

--- scraper.py
+++ scraper.py
@@ -20,8 +20,8 @@ class Scraper:
             if (item.find('a', {'class': 's-item__link'})['href'] != "https://ebay.com/itm/123456?hash=item28caef0a3a:g:E3kAAOSwlGJiMikD&amdata=enc%3AAQAHAAAAsJoWXGf0hxNZspTmhb8%2FTJCCurAWCHuXJ2Xi3S9cwXL6BX04zSEiVaDMCvsUbApftgXEAHGJU1ZGugZO%2FnW1U7Gb6vgoL%2BmXlqCbLkwoZfF3AUAK8YvJ5B4%2BnhFA7ID4dxpYs4jjExEnN5SR2g1mQe7QtLkmGt%2FZ%2FbH2W62cXPuKbf550ExbnBPO2QJyZTXYCuw5KVkMdFMDuoB4p3FwJKcSPzez5kyQyVjyiIq6PB2q%7Ctkp%3ABlBMULq7kqyXYA"):
                 product = {
                     'title': item.find('div', {'class': 's-item__title'}).text,
-                    'soldprice': float(item.find('span', {'class': 's-item__price'}).text.replace('£', '').replace(',', '').strip()),
-                    'timeleft': item.find('span', {'class': 's-item__time-left'}).text,
+                    'price': float(item.find('span', {'class': 's-item__price'}).text.replace('£', '').replace(',', '').strip()),
+                    'time_left': item.find('span', {'class': 's-item__time-left'}).text,
                     'bids': item.find('span', {'class': 's-item__bids'}).text,
                     'link': item.find('a', {'class': 's-item__link'})['href']
                 }
@@ -30,15 +30,15 @@ class Scraper:
 
     def return_data(self):
         soup = Scraper.get_data(self.url)
-        print(type(Scraper.parse(soup))) # dictionary inside list
-        print("\n\n")
-        print(Scraper.parse(soup)[0].get('title'))
-        print(Scraper.parse(soup)[0].get('soldprice'))
-        print(Scraper.parse(soup)[0].get('timeleft'))
-        print(Scraper.parse(soup)[0].get('bids'))
-        print(Scraper.parse(soup)[0].get('link'))
+        # print(type(Scraper.parse(soup))) # dictionary inside list
+        # print("\n\n")
+        # print(Scraper.parse(soup)[0].get('title'))
+        # print(Scraper.parse(soup)[0].get('price'))
+        # print(Scraper.parse(soup)[0].get('time_left'))
+        # print(Scraper.parse(soup)[0].get('bids'))
+        # print(Scraper.parse(soup)[0].get('link'))
 
-        return type(Scraper.parse(soup)[0])
+        return Scraper.parse(soup)
         # return Scraper.parse(soup)[0].get('title')
 
 ''' test_scraper = Scraper("https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=perfume&_sacat=0&rt=nc&LH_Auction=1")
--- ui.py
+++ ui.py
@@ -13,7 +13,7 @@ url1 = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw="
 url2 = "&_sacat=0&rt=nc&LH_Auction=1"
 
 def close_app():
-    window.destroy()
+    exit()
 
 def search():
     # get inputs to scrape
@@ -24,89 +24,104 @@ def search():
     print("search: " + search_product1)
     scraper1 = Scraper(url1 + search_product1 + url2) # concatenate strings to include search value
     data_list = scraper1.return_data()
-    print("test here")
-    print(data_list)
+    create_table(data_list)
+
+#data = [{'Geeks': 'dataframe', 'For': 'using', 'geeks': 'list', 'Portal': 10000}, {'Geeks':10, 'For': 20, 'geeks': 30}]
+
+def create_table(data_list):
+    # adding a table
+    # ---------------------------------------------------------------------------------------------------- #
+
+    # define columns
+    columns = ('title', 'price', 'time_left', 'bids', 'link')
+
+    tree = ttk.Treeview(frame_bottom, columns=columns, show='headings')
+
+    # delete previous records
+    for item in tree.get_children():
+        tree.delete(item)
+
+    # define headings
+    tree.heading('title', text='Product Title')
+    tree.heading('price', text='Price')
+    tree.heading('time_left', text='Time Left')
+    tree.heading('bids', text='Bids')
+    tree.heading('link', text='Link')
+
+    # generate sample data
+    # soup[0] - DICT
+    # soup - LIST
+    # data = [{'Geeks': 'dataframe', 'For': 'using', 'geeks': 'list', 'Portal': 10000}, {'Geeks':10, 'For': 20, 'geeks': 30}] 
+
+    # ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91') = TUPLE
+    # ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91'), ('title 92', 'price 92', 'time_left 92', 'bids 92', 'link 92') = LIST
+    products = []
+    # for i in data_list:
+    #     products.append(('title ', 'price ', 'time_left ', 'bids ', 'link '))
+    
+    for i in range(len(data_list)):
+        products.append((data_list[i].get('title'), data_list[i].get('price'), data_list[i].get('time_left'), data_list[i].get('bids'), data_list[i].get('link')))
+    
+
+    # add data to the treeview
+    for product in products:
+        tree.insert('', END, values=product)
+
+    tree.grid(row=0, column=0, sticky='nsew') # adds table to the grid
+
+    # add a scrollbar
+    scrollbar = ttk.Scrollbar(frame_bottom, orient=VERTICAL, command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+    scrollbar.grid(row=0, column=1, sticky='ns')
+
+    # ---------------------------------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+    window = Tk()
+    # window.state("zoomed") # makes full screen with title bar and taskbar
+    window.title("Ecommerce-Web-Scraper")
+
+    # creating grid for gui
+    frame_header = Frame(window, borderwidth=2, pady=2)
+    frame_centre = Frame(window, borderwidth=2, pady=5)
+    frame_bottom = Frame(window, borderwidth=2, pady=5)
+
+    frame_header.grid(row=0, column=0)
+    frame_centre.grid(row=1, column=0)
+    frame_bottom.grid(row=2, column=0)
+
+    header = Label(frame_header, text="Ecommerce Web Scraper", bg='grey', fg='black', height='3', width='50', font=("Helvetica 16 bold"))
+    header.grid(row=0, column=0)
+
+    frame_main_1 = Frame(frame_centre, borderwidth=2, relief='sunken')
+    frame_main_2 = Frame(frame_centre, borderwidth=2, relief='raised')
+
+    search_product = Label(frame_main_1, text="Search product: ")
+    # NEED TO ADD SEND BUTTON
+    search_product1 = StringVar() # ??
+    search_product_entry = Entry(frame_main_1, textvariable = search_product1, width=40) # width=40 changes the width of the entry box
+    search_button = Button(frame_main_1, text="Search", command=search, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
 
 
-window = Tk()
-# window.state("zoomed") # makes full screen with title bar and taskbar
-window.title("Ecommerce-Web-Scraper")
+    #adding send button
+    close_button = Button(frame_main_1, text="Close", command=close_app, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
 
-# creating grid for gui
-frame_header = Frame(window, borderwidth=2, pady=2)
-frame_centre = Frame(window, borderwidth=2, pady=5)
-frame_bottom = Frame(window, borderwidth=2, pady=5)
-
-frame_header.grid(row=0, column=0)
-frame_centre.grid(row=1, column=0)
-frame_bottom.grid(row=2, column=0)
-
-header = Label(frame_header, text="Ecommerce Web Scraper", bg='grey', fg='black', height='3', width='50', font=("Helvetica 16 bold"))
-header.grid(row=0, column=0)
-
-frame_main_1 = Frame(frame_centre, borderwidth=2, relief='sunken')
-frame_main_2 = Frame(frame_centre, borderwidth=2, relief='raised')
-
-search_product = Label(frame_main_1, text="Search product: ")
-# NEED TO ADD SEND BUTTON
-search_product1 = StringVar() # ??
-search_product_entry = Entry(frame_main_1, textvariable = search_product1, width=40) # width=40 changes the width of the entry box
-search_button = Button(frame_main_1, text="Search", command=search, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
+    data = [{'title': 'car 1', 'price': '1', 'time_left': 'time 1', 'bids': "1", 'link': 'link test 1'}, {'title': 'car 2', 'price': '2', 'time_left': 'time 2', 'bids': "2", 'link': 'link test 2'}] 
+    create_table(data)
 
 
-#adding send button
-close_button = Button(frame_bottom, text="Close", command=close_app, bg='dark green', fg='white', relief='raised', width=10, font=('Helvetica 9 bold')) # command=search_products - search_products is function
-close_button.grid(column=0, row=0, sticky='w', padx=100, pady=2)
-
-# adding a table
-# ---------------------------------------------------------------------------------------------------- #
-
-# define columns
-columns = ('title', 'price', 'time_left', 'bids', 'link')
-
-tree = ttk.Treeview(frame_bottom, columns=columns, show='headings')
-
-# define headings
-tree.heading('title', text='Product Title')
-tree.heading('price', text='Price')
-tree.heading('time_left', text='Time Left')
-tree.heading('bids', text='Bids')
-tree.heading('link', text='Link')
-
-# generate sample data
-# soup - LIST
-
-# ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91') = TUPLE
-# ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91'), ('title 91', 'price 91', 'time_left 91', 'bids 91', 'link 91') = LIST
-contacts = []
-for n in range(1, 100):
-    contacts.append((f'title {n}', f'price {n}', f'time_left {n}', f'bids {n}', f'link {n}'))
-
-# add data to the treeview
-for contact in contacts:
-    tree.insert('', END, values=contact)
-
-tree.grid(row=0, column=0, sticky='nsew') # adds table to the grid
-
-# add a scrollbar
-scrollbar = ttk.Scrollbar(frame_bottom, orient=VERTICAL, command=tree.yview)
-tree.configure(yscroll=scrollbar.set)
-scrollbar.grid(row=0, column=1, sticky='ns')
-
-# ---------------------------------------------------------------------------------------------------- #
+    # adding widgets to the grid
+    frame_main_1.pack(fill='x', pady=2)
+    frame_main_2.pack(fill='x', pady=2)
+    search_product.pack(side='left')
+    search_product_entry.pack(side='left', padx=1)
+    search_button.pack(side='left', padx=1)
+    close_button.pack(side='left', padx=1)
 
 
-# adding widgets to the grid
-frame_main_1.pack(fill='x', pady=2)
-frame_main_2.pack(fill='x', pady=2)
-search_product.pack(side='left')
-search_product_entry.pack(side='left', padx=1)
-search_button.pack(side='left', padx=1)
+    window.configure(width=500, height=750, bg=BG_COLOR) # window.geometry("500x750")
 
 
-window.configure(width=500, height=750, bg=BG_COLOR) # window.geometry("500x750")
-
-
-# all widgets will be here
-# Execute Tkinter
-window.mainloop()
+    # all widgets will be here
+    # Execute Tkinter
+    window.mainloop()
